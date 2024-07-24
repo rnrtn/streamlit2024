12d2713c85b8ab2e8bdae5afaeba9dec48f23538
rnrtn <infortjsw@gmail.com>
2024-07-24 12:01:21 +0900

    dice

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import random
 st.header('주사위 게임', divider='rainbow')
 clicked = st.button('주사위던지기', type='primary')
 if clicked :
-    n = random.randint(1, 6)
+    n = random.randint(1,6)
     st.image(f'./img/{n}.png')
